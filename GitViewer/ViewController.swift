5041a99942e1d87cfd91685a4d6a9006c6c74b94
Khalid Rahman <khalid@codegurus.co.uk>
2019-12-29 00:28:10 +0000

    Initial Commit

---
+++ GitViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitViewer
+//
+//  Created by Khalid Rahman on 29/12/2019.
+//  Copyright © 2019 Khalid Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
