b259df589e18f13837460dc6adae621339cbc0dc
Khalid Rahman <khalid@codegurus.co.uk>
2019-12-29 04:45:31 +0000

    added custom button and textfield

---
+++ GitViewer/CustomViews/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GitViewer
+//
+//  Created by Khalid Rahman on 29/12/2019.
+//  Copyright © 2019 Khalid Rahman. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColour: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor                        = backgroundColor
+        self.setTitle(title, for: .normal)
+        
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius                          = 10
+        titleLabel?.textColor                       = .white
+        titleLabel?.font                            = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints   = false
+    }
+}
--- GitViewer/SceneDelegate.swift
+++ GitViewer/SceneDelegate.swift
@@ -13,11 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let windowScene           = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = createTabBar()
+        window                          = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene             = windowScene
+        window?.rootViewController      = createTabBar()
         window?.makeKeyAndVisible()
     }
 
